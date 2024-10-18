ab8273e51b858723e16c81d966f510af6cece98f
Tanny <lynxy86@mail.ru>
2024-10-18 19:43:47 +0300

    2tasks finished

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,42 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	const discriminant = Math.pow(b, 2) - 4 * a * c;
+
+	if (discriminant < 0) {
+		return [];
+	} else if (discriminant === 0) {
+		const root = -b / (2 * a);
+		return [root];
+	} else {
+		const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		return [root1, root2];
+	}
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+	percent = parseFloat(percent);
+	contribution = parseFloat(contribution);
+	amount = parseFloat(amount);
+	countMonths = parseInt(countMonths);
+
+	if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+		return false;
+	}
+
+	let monthlyInterestRate = (percent / 100) / 12;
+	let loanBody = amount - contribution;
+
+	if (loanBody <= 0) {
+		return 0;
+	}
+
+
+	let monthlyPayment = loanBody * (monthlyInterestRate + (monthlyInterestRate / (Math.pow((1 + monthlyInterestRate), countMonths) - 1)));
+	let totalPayment = monthlyPayment * countMonths;
+
+	return Math.round(totalPayment * 100) / 100;
 }
