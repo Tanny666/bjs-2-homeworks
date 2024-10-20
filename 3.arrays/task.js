ac3c28e070fdeafef7665bc26cdca92248b17f08
Tanny <lynxy86@mail.ru>
2024-10-20 16:57:07 +0300

    tasks finished

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+
+	if (arr1.length !== arr2.length) {
+
+		return false;
+	}
+
+	return arr1.every((value, index) => value === arr2[index]);
 }
 
+
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+	const averageAge = users
+		.filter(user => user.gender === gender)
+		.map(user => user.age)
+		.reduce((sum, age, _, array) => sum + age / array.length, 0);
+
+	return averageAge;
 }
